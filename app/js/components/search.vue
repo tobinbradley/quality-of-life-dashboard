5f48bfd614c8649ea367315171c2b24316a33748
tobinbradley <tobin.bradley@gmail.com>
2016-09-22 16:32:31 -0400

    verbiage tweak

--- app/js/components/search.vue
+++ app/js/components/search.vue
@@ -5,7 +5,7 @@
             <label class="mdl-textfield__label" for="sample3">Search...</label>
         </div>
         <div class="search-instructions" v-if=" privateState.results.neighborhood.length === 0 &&  privateState.results.zipcode.length === 0 && privateState.results.address.length === 0">
-            Start typing a <span class="tooltip" title="{{{ privateState.neighborhoodDefinition }}}">{{ privateState.neighborhoodDescriptor }}</span> id, address, or zip code.
+            Enter a <span class="tooltip" title="{{{ privateState.neighborhoodDefinition }}}">{{ privateState.neighborhoodDescriptor }}</span> id, address, or zip code.
         </div>
         <div class="search-results">
             <ul v-for="n in privateState.results.neighborhood">
@@ -110,7 +110,7 @@ export default {
                   })
                   .then(function (response) {
                        _this.privateState.results.address = [];
-                      if (response.data.length > 0) {                         
+                      if (response.data.length > 0) {
                           for (let i = 0; i < response.data.length; i++) {
                               _this.privateState.results.address.push({ 'label': response.data[i].address, 'lng':  response.data[i].lng, 'lat': response.data[i].lat});
                           }
