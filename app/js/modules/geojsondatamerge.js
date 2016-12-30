5bcb5497c268e66dbe0c6bbf521f1d03492026a8
tobinbradley <tobin.bradley@gmail.com>
2016-12-30 12:59:44 -0500

    added 3d value calculation

--- app/js/modules/geojsondatamerge.js
+++ app/js/modules/geojsondatamerge.js
@@ -1,11 +1,18 @@
 import isNumeric from './isnumeric';
+import {valsToArray} from './metric_calculations';
 
 function geojsonDataMerge(g, d, y) {
+    // get array max
+    let valsArray = valsToArray(d, [y], Object.keys(d));
+    let heightFactor = 5000 / Math.max.apply(Math, valsArray);
+
     for (let i = 0; i < g.features.length; i++) {
         if (isNumeric(d[g.features[i].properties.id][`y_${y}`])) {
             g.features[i].properties.choropleth = d[g.features[i].properties.id][`y_${y}`];
+            g.features[i].properties.height = d[g.features[i].properties.id][`y_${y}`] * heightFactor;
         } else  {
             g.features[i].properties.choropleth = 'null';
+            g.features[i].properties.height = 'null';
         }
     }
     return g;
