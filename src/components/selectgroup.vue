2a4eef7854fa90df5eb8dc7fdc0848c0c982c621
tobinbradley <tobin.bradley@gmail.com>
2018-12-13 13:45:11 -0500

    add verbiage because people

--- src/components/selectgroup.vue
+++ src/components/selectgroup.vue
@@ -1,5 +1,6 @@
 <template>
   <p v-if="selectGroup && typeof selectGroup === 'object'">
+    Approximate: 
     <template v-for="group, key, index in selectGroup">
     <a href="javascript:void(0)" class="selectgroup-link" v-bind:id="`selectgroup-${index}`">{{key}}</a>
     <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu" v-bind:for="`selectgroup-${index}`">                       
