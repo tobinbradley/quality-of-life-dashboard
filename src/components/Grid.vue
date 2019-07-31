f70a82cecdf09f0249ac22727f696bffe6219f04
tobinbradley <tobin.bradley@gmail.com>
2019-07-31 12:54:36 -0400

    remove unused css

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -84,9 +84,7 @@
   flex: 0 0 380px;
   height: 400px;
   border-radius: 2px;
-  box-shadow: 0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12);
   background: #fff;
-  box-sizing: border-box;
   overflow: auto;
   overflow-x: hidden;
   transition: transform 0.3s;
