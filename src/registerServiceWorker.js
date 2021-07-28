5c2659335922e08b340deaa82b5fad8d9c55e6be
tobinbradley <tobin.bradley@gmail.com>
2021-07-28 11:10:55 -0400

    updated sw registration with update prompt

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,26 +1,39 @@
-/* eslint-disable no-console */
+import Vue from 'vue';
+import VueToast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
 
-import { register } from 'register-service-worker'
+Vue.use(VueToast);
+let newWorker
 
-if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    cached () {
-      console.log('Content has been cached for offline use.')
-    },
-    updated () {
-      console.log('New content is available; please refresh.')
-    },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error (error) {
-      console.error('Error during service worker registration:', error)
-    }
-  })
+// Register service worker and show update notice if new content
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js').then(reg => {
+    reg.addEventListener('updatefound', () => {
+
+      // An updated service worker has appeared in reg.installing!
+      newWorker = reg.installing;
+
+      newWorker.addEventListener('statechange', () => {
+
+        // Has service worker state changed?
+        switch (newWorker.state) {
+          case 'installed':
+
+            // There is a new service worker available, show the notification
+            if (navigator.serviceWorker.controller) {
+              console.log('New content is available, please refresh.')
+              newWorker.postMessage({ type: 'SKIP_WAITING' })
+              Vue.$toast.open({
+                message: 'The Explorer has been updated! .',
+                type: 'info',
+                duration: 6000,
+                onClick: () => { location.reload() }
+              });
+            }
+
+            break;
+        }
+      });
+    });
+  });
 }
